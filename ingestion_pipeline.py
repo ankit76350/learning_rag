5d478428d9e3e03c92f2b8c036cbbfd4cb67b004
Ankit Kumar <krankit01012003@gmail.com>
2026-08-19 07:08:25 +0530

    I am learning the how to make the veector and store it locally using chroma db

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -66,6 +66,25 @@ def split_documents(documents, chunk_size=1000, chunk_overlap=0):
     
     return chunks
 
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+    """Create and persist ChromaDB vector store"""
+    print("Creating embeddings and storing in ChromaDB...")
+        
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+    
+    # Create ChromaDB vector store
+    print("--- Creating vector store ---")
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_directory, 
+        collection_metadata={"hnsw:space": "cosine"}
+    )
+    print("--- Finished creating vector store ---")
+    
+    print(f"Vector store created and saved to {persist_directory}")
+    return vectorstore
+
 def main():
     print("Main Function")
 
@@ -77,3 +96,7 @@ if __name__ == "__main__":
 
     # Step 2: Split into chunks
     chunks = split_documents(documents)
+
+
+    # # Step 3: Create vector store
+    vectorstore = create_vector_store(chunks)
